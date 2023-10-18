2b62843e5e0f3eba6d2d3e52565ae09004e4b5bf
chrissuttondev <>
2023-10-18 12:46:49 +0000

    Wrapped input and if statement in while loop

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ choices = ["r", "p", "s"]
 cc = "r"
 # Input validation need. If uci not r,p or s
 
-uci = input("make your guess by typing 'r', 'p' or 's':")
-uc = uci.lower()
-if uci not in choices:
-    print("Please try again. Enter 'r', 'p' or 's'")
+while True:
+    uci = input("make your guess by typing 'r', 'p' or 's':")
+    uc = uci.lower()
+    if uci not in choices:
+        print("Please try again. Enter 'r', 'p' or 's'")
+    else:
+        break
+
+print(uc)
