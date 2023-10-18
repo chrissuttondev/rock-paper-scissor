f97461f000bef91038a484f7a655470fc6e2d771
chrissuttondev <>
2023-10-18 13:28:20 +0000

    Added if / elif statements to compare uc against cc

--- run.py
+++ run.py
@@ -1,16 +1,35 @@
 import random
 # Rock, paper, scissors game vs computer
+# Rock
 
 choices = ["r", "p", "s"]
 cc = "r"
-# Input validation need. If uci not r,p or s
 
 while True:
-    uci = input("make your guess by typing 'r', 'p' or 's':")
-    uc = uci.lower()
-    if uci not in choices:
-        print("Please try again. Enter 'r', 'p' or 's'")
-    else:
-        break
+    # while loop checks if input. Loops is broken when input is r, s or p.
+    while True:
+        uci = input("make your guess by typing 'r', 'p' or 's':")
+        uc = uci.lower()
+        if uci not in choices:
+            print("Please try again. Enter 'r', 'p' or 's'")
+        else:
+            break
+    print(uc)
+    print(cc)
 
-print(uc)
+    # if / elif statement to compare uc and cc
+
+    if uc == cc:
+        print("Draw")
+    elif uc == "r" and cc == "p":
+        print("Computer wins")
+    elif uc == "r" and cc == "s":
+        print("You win")
+    elif uc == "p" and cc == "r":
+        print("You win")
+    elif uc == "p" and cc == "s":
+        print("Computer wins")
+    elif uc == "s" and cc == "r":
+        print("Computer wins")
+    elif uc == "s" and cc == "p":
+        print("You win")
